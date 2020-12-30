cdb14531bbc837f549f606a3c4b5e8aee39982da
Halyna Pryn <prynhalyna@gmail.com>
2020-12-30 20:31:05 +0100

    Chahges 30,12

--- build/main.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";console.log("Hello from index.js"),fetch("https://restcountries.eu/rest/v2/name/eesti").then((function(e){return e.json()})).then((function(e){return console.log(e)}))})();
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
+console.log('Hello from index.js-1');
+
+var debounce = require('debounce');
 // var debounce = require('lodash.debounce');
+// console.log(debounce);
 
-// const inputRef = document.querySelector('#input');
+const inputRef = document.querySelector('#exampleDataList');
 
-// inputRef.addEventListener(
-//   'input',
-//   _.debounce(event => {
-//     console.log(event.data);
-//   }),
-// );
-console.log("Hello from index.js");
+inputRef.addEventListener(
+  'input',
+  debounce(event => {
+    console.log(event.data);
+  }, 500),
+);
 
-fetch('https://restcountries.eu/rest/v2/name/eesti')
-  .then(res => res.json())
-  .then(data => console.log(data));
+console.log('Hello from index.js-2');
+
+// fetch('https://restcountries.eu/rest/v2/name/eesti')
+//   .then(res => res.json())
+//   .then(data => console.log(data));
